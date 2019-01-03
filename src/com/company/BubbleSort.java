318b18ee77786763caa2c34d278c09a09cd30831
BT_1N3_18 <jzhang0688@bths.edu>
2019-01-03 08:40:39 -0500

    ss

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -1,6 +1,23 @@
 package com.company;
 
 public class BubbleSort {
-    int a,b;
-    while (a < testArr.length)
+    public static int[] bubbleSort(int[] arr){
+        for(int n = 0; n < arr.length; n++)
+        {
+            if(arr[n] > arr[n+1])
+            {
+                swap(arr, n, n+1);
+                n = -1;
+            }
+        }
+        return arr;
+    }
+
+
+    public static void swap(int[]testArr,int x,int y){
+        int temp;
+        temp = testArr[x];
+        testArr[x]=testArr[y];
+        testArr[y]=temp;
+    }
 }
