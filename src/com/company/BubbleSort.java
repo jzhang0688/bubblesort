6b0deb23ab2849e2c1cf2d7f534e443881d1ae94
BT_1N3_18 <jzhang0688@bths.edu>
2019-01-04 08:40:34 -0500

    sortingUtils

--- src/com/company/BubbleSort.java
+++ src/com/company/BubbleSort.java
@@ -1,23 +1,19 @@
 package com.company;
 
+
 public class BubbleSort {
 
-    public static int[] bubbleSort(int[] testArr){
-        for(int count = 0; count < testArr.length; count++) {
-            for (int n = 0; n < testArr.length - 1; n++) {
-                if (testArr[n] > testArr[n + 1]) {
-                    swap(testArr, n, n + 1);
+    public static int[] bubbleSort(int[] Arr){
+        for(int count = 0; count < Arr.length; count++) {
+            for (int n = 0; n < Arr.length - 1; n++) {
+                if (Arr[n] > Arr[n + 1]) {
+                    SortingUtils.swap(Arr, n, n + 1);
                 }
             }
         }
-        return testArr;
+        return Arr;
     }
 
 
-    public static void swap(int[]testArr,int x,int y){
-        int temp;
-        temp = testArr[x];
-        testArr[x]=testArr[y];
-        testArr[y]=temp;
-    }
+
 }
